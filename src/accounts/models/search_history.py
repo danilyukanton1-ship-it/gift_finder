ba85e73e23ac9eadfcecf3b0ed88e82a8ba15f8c
Anton Danilyuk <danilyukanton1@gmail.com>
2026-04-28 22:19:16 +0300

    upgrade SearchHistory Model (add related_name for Foreign Keys and add class Meta with verbose_name and ordering)

--- src/accounts/models/search_history.py
+++ src/accounts/models/search_history.py
@@ -6,9 +6,18 @@ from gifts.models import Option
 
 class SearchHistory(BaseModel):
 
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="search_histories"
+    )
 
-    options = models.ManyToManyField(Option, blank=True)
+    options = models.ManyToManyField(
+        Option, blank=True, related_name="search_histories"
+    )
 
     def __str__(self):
         return f'{self.user.username} - {self.created_at.strftime("%Y-%m-%d %H:%M")}'
+
+    class Meta:
+        verbose_name = "Search History"
+        verbose_name_plural = "Search Histories"
+        ordering = ("-created_at",)
