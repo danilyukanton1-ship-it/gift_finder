c51843abfa62de9e6c655676f5f622a7883e8837
Anton Danilyuk <danilyukanton1@gmail.com>
2026-04-28 17:03:48 +0300

    fix BaseModel for successful implementation to existing models

--- src/base/models.py
+++ src/base/models.py
@@ -1,10 +1,12 @@
+from django.utils import timezone
+
 from django.db import models
 
 
 class BaseModel(models.Model):
 
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
 
     class Meta:
         abstract = True
