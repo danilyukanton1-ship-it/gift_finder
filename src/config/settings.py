c2121814f0181ea4bf48682c69444814c9f6adf2
Anton Danilyuk <danilyukanton1@gmail.com>
2026-05-08 20:10:06 +0300

    add allowed hosts to settings

--- src/config/settings.py
+++ src/config/settings.py
@@ -16,7 +16,7 @@ from django.urls import reverse_lazy
 from datetime import timedelta
 
 env = environ.Env()
-environ.Env.read_env(env_file=".env")
+environ.Env.read_env(env_file="../../.env")
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -26,7 +26,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 # Application definition
 
