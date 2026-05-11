7c44615212c41b40ac4460fca34949657628ed2f
Anton Danilyuk <danilyukanton1@gmail.com>
2026-05-12 00:37:24 +0300

    refactory direction_view_services

--- src/gifts/services/direction_view_services.py
+++ src/gifts/services/direction_view_services.py
@@ -20,27 +20,18 @@ class DirectionViewService:
         return None
 
     def save_search_history(self):
-        if self.request.user.is_authenticated:
+        if self.request.user.is_authenticated and self.option_ids:
             history = SearchHistory.objects.create(user=self.request.user)
             history.options.set(self.option_ids)
 
     def get_recommendations(self):
-        if self._result is None:
-            engine = GiftSearchService(self.option_ids)
-            self._result = engine.get_result()
-            # Отладка
-            print(f"=== DEBUG ===")
-            print(f"option_ids: {self.option_ids}")
-            print(f"result keys: {list(self._result.keys())}")
-            print(f"result data: {self._result}")
-            for dir_id, data in self._result.items():
-                print(f"Direction {dir_id}: {len(data.get('products', []))} products")
+        engine = GiftSearchService(self.option_ids)
+        self._result = engine.get_result()
         return self._result
 
     def result_to_serializable(self):
         if self._result is None:
             self._result = self.get_recommendations()
-
         serializable_result = {}
         for direction_id, data in self._result.items():
             serializable_result[direction_id] = {
@@ -49,22 +40,16 @@ class DirectionViewService:
                 "product_count": len(data["products"]),
                 "top_products": data["top_products"],
             }
-        self._serializable_result = serializable_result
         return serializable_result
 
     def serialize_and_store_in_session(self):
         if self._serializable_result is None:
-            serializable_result = self.result_to_serializable()
-        else:
-            serializable_result = self._serializable_result
-        serialized = serialize_products_by_direction(serializable_result)
+            self._serializable_result = self.result_to_serializable()
+        serialized = serialize_products_by_direction(self._serializable_result)
         self.request.session["all_products"] = serialized
         return serialized
 
     def prepare_directions_data(self):
-        if self._result is None:
-            self._result = self.get_recommendations()
-
         directions_data = []
         for direction_id, data in self._result.items():
             directions_data.append(
@@ -84,8 +69,6 @@ class DirectionViewService:
             return redirect_response, None, False
 
         self.save_search_history()
-        self.get_recommendations()
-        self.result_to_serializable()
         self.serialize_and_store_in_session()
 
         direction_data = self.prepare_directions_data()
